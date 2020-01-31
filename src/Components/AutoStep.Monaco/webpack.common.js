55c859572919eadf6255f5782bf97a5f060a36ef
alistairjevans <alistairjevans@gmail.com>
2020-01-31 11:42:17 +0000

    Go back to relative path for entry point. Case sensitivity issue fixed remotely.

--- src/Components/AutoStep.Monaco/webpack.common.js
+++ src/Components/AutoStep.Monaco/webpack.common.js
@@ -3,7 +3,7 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
   entry: {
-     app: path.resolve(__dirname, 'src') + "/MonacoInterop.ts",
+     app: "./MonacoInterop.ts",
     "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js"
   },
   resolve: {
